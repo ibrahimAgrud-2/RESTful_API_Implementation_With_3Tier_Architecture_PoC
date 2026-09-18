18a0670614e7ae6e68c6024141b289d9fcbe1ebc
ibrahim <ibrahimagrud7@gmail.com>
2026-09-18 15:56:04 +0300

    API: get passed student complete

--- API_Implementation/Controllers/StudentController.cs
+++ API_Implementation/Controllers/StudentController.cs
@@ -14,7 +14,7 @@ namespace API_Implementation.Controllers
     public class StudentController : ControllerBase
     {
 
-        //Bu da tamamen çalışır ama 2.yöntem daha profesyoneldir.
+        /*Bu da tamamen çalışır ama 2.yöntem daha profesyoneldir.
         //Çünkü bu yöntemde status code'u geri dönerme gibi bir şansımız yok.
         //mesela üstüden listesi boşsa notFound, veya client'in izni yoksa 
         //Forbidden gibi status dönderebilmek için action result kullanmalıyız
@@ -22,13 +22,13 @@ namespace API_Implementation.Controllers
         //public List<Student> GetStudentList()
         //{
         //    return StudentDataSimulation.StudentList;
-        //}
+        //}*/
 
 
-        //Yukarda açıkladığımız sebepten ötürü veri döndereceğin zaman ActionResult ile döndermelisin
+        /*Yukarda açıkladığımız sebepten ötürü veri döndereceğin zaman ActionResult ile döndermelisin
         //Action Result geriye veriyi ve o request'in status code'unu action result kutusu içinde gönderir kodu dönderir  
         //IEnumarable ise client kısmı için list, array, dictionary fark etmeyeceği, onu sadece JSON formatında veri alacağı için hangi türden veri döndürdüğümüzün pek önemi yok. Daha doğrusu sadeec List<Student> demek yerine durumu daha geneleştiriyoruz. IEnumarable<student> diyoruz. IEnumarable zaten list'in daha geniş halidir. Bu durumda biz geriye bir koleksiyon döncekek. Ne olduğu pek önemli diğer onu al ve JSON olarak istediğini yap şeklinde düşünüyoruz.
-        //!!!! Arka planda ASP framework JSON serialization ile veriyi JSON'a çeviriyor
+        //!!!! Arka planda ASP framework JSON serialization ile veriyi JSON'a çeviriyor*/
         [HttpGet("All")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -43,12 +43,8 @@ namespace API_Implementation.Controllers
 
             return Ok(StudentList);
         }
-
-
-
+        
         //isimlendirmeler ile (all, passed) attriburte'lar birbirinden farklı olmuş oldu. Ama daha okunaklı URL'lere adına Attrüburlara her zaman değişkenlerde olduğu gibi alakalı isim vermek gerekir.
-
-
         [HttpGet("Passed")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
